4e15c97232977d230340b7bb84f014a4f560ed29
Jayr <jayralencarpereira@gmail.com>
2022-10-21 12:49:51 +0000

    qasper

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 # dataset download  
 
+# iirc
 wget https://iirc-dataset.s3.us-west-2.amazonaws.com/iirc_train_dev.tgz -P data
 wget https://iirc-dataset.s3.us-west-2.amazonaws.com/context_articles.tar.gz -P data
 wget https://iirc-dataset.s3.us-west-2.amazonaws.com/iirc_test.json -P data
@@ -8,6 +9,11 @@ cd data
 tar -xf context_articles.tar.gz
 tar -xf iirc_train_dev.tgz
 
+
+# qasper
+
+wget https://qasper-dataset.s3.us-west-2.amazonaws.com/qasper-test-and-evaluator-v0.3.tgz
+tar -xf qasper-test-and-evaluator-v0.3.tgz
 cd ..
 
 wget https://storage.googleapis.com/ai2i/strategyqa/data/strategyqa_dataset.zip -P data
